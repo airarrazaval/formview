d6091fbde456afbac413b2e8f7fd292554694419
Matteus Brevik <matteus.brevik@icloud.com>
2018-04-12 14:53:00 +0000

    Updated 'scripts/app.js' via CloudCannon

--- scripts/app.js
+++ scripts/app.js
@@ -20,6 +20,10 @@ function getFormConfig() {
     }
 }
 
+function onFormSubmit(title = 'Sent!', message = 'Your submission has been sent!') {
+    
+}
+
 window.onload = function() {
     let config = getFormConfig();
     
@@ -41,10 +45,15 @@ window.onload = function() {
             i18n: i18n
         }
     }).then(function(options) {
-        console.log(options);
         Formio.createForm(document.getElementById('formio'), config.project.concat('/', config.formPath), options).then(function(form) {
             form.language = options.language;
-        })
+            form.on('submitDone', (submission) => {
+                let title = options.i18n[options.language]['Sent!'] || 'Sent!';
+                let msg = options.i18n[options.language]['Your submission has been sent!'] || 'Your submission has been sent!';
+                let formio = document.getElementById('formio');
+                formio.innerHTML = `<div class="alert alert-success"><strong><span class="glyphicon glyphicon-ok-sign"></span> ${title}</strong> ${msg}</div>`;
+            });
+        });
     }).catch(function(error) {
         console.log('form rendering error: ', error);
     });
