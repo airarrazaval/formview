b6e78014747fd01480e574c5ed66a0f3beb66205
Matteus Brevik <matteus.brevik@icloud.com>
2018-04-12 14:34:20 +0000

    Updated 'scripts/app.js' via CloudCannon

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,8 @@
 function getParameterByName(name, url) {
     if (!url) url = window.location.href;
     name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-        console.log(results);
+    let regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
+    let results = regex.exec(url);
     if (!results) return null;
     if (!results[2]) return '';
     return decodeURIComponent(results[2].replace(/\+/g, " "));
@@ -27,29 +26,23 @@ window.onload = function() {
     let translationsUrl = config.project.concat('/', config.translations.path, '/submission');
     let filter = '?limit=1000&select=data.label,data.' + config.translations.language;
     
-    console.log('getting translations...');
     Formio.request(translationsUrl + filter, 'get').then(function(items) {
-        console.log('tarnslations received! total labels: ', items.length);
         let lang = config.translations.language;
         let i18n = {};
         i18n[lang] = {}
         for(let i = 0; i < items.length; i++) {
             let item = items[i];
-            if (item.hasOwnProperty(lang)) {
-                config.translations.i18n[lang] = item[lang];
+            if (item.data.hasOwnProperty(lang)) {
+                config.translations.i18n[lang][item.data.label] = item.data[lang];
             }
         }
-        console.log(i18n);
         return {
             language: lang,
             i18n: i18n
         }
     }).then(function(options) {
-        console.log('getting form...', options);
         Formio.createForm(document.getElementById('formio'), config.project.concat('/', config.formPath), options).then(function(form) {
-            console.log('form received!')
             form.language = options.language;
-            console.log('form language set: ', options.language);
         })
     }).catch(function(error) {
         console.log('form rendering error: ', error);
