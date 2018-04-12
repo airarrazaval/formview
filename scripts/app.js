e051abf15a6ecf15fdc5505ec55a4008051aaa37
Mattues Brevik <matteus.brevik@icloud.com>
2018-04-12 19:38:58 +0200

    migrated to vuejs app

--- scripts/app.js
+++ scripts/app.js
@@ -1,64 +1,70 @@
-function getParameterByName(name, url) {
-    if (!url) url = window.location.href;
-    name = name.replace(/[\[\]]/g, "\\$&");
-    let regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
-    let results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
-}
-
-function getFormConfig() {
-    let configString = atob(getParameterByName('form'));
+console.log('starting')
+var app = new Vue({
+  el: '#app',
+  data () {
     return {
-        project: getParameterByName('project', configString) || 'https://examples.form.io',
-        formPath: getParameterByName('path', configString) || 'example',
-        translations: {
-            language: getParameterByName('lang') || 'en',
-            path: getParameterByName('i18n', configString) || 'translations'
-        }
+      loading: true,
+      showHeader: true,
+      project: 'https://examples.form.io',
+      formPath: 'example',
+      translationsPath: 'translations',
+      options: {
+        language: 'en',
+        i18n: {}
+      }
     }
-}
-
-window.onload = function() {
-    let header = document.getElementById('header');
-    let showHeader = parseInt(getParameterByName('header'));
-    
-    if (!showHeader) {
-        header.remove();
+  },
+  methods: {
+    getParameterByName (name, url) {
+      if (!url) {
+        url = window.location.href
+      }
+      name = name.replace(/[\[\]]/g, "\\$&")
+      let regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)")
+      let results = regex.exec(url)
+      if (!results) { 
+        return null
+      }
+      if (!results[2]) {
+        return ''
+      }
+      return decodeURIComponent(results[2].replace(/\+/g, " "))
+    },
+    createTranslations () {
+      let filter = '?limit=1000&select=data.label,data.'.concat(this.options.language)
+      let url = this.project.concat('/', this.translationsPath, '/submission')
+      return Formio.request(url + filter, 'get').then((items) => {
+        for (let i = 0; i < items.length; i++) {
+          let item = items[i]
+          if (item.data.hasOwnProperty(this.options.language)) {
+            this.options.i18n[this.options.language][item.data.label] = item.data[this.options.language]
+          }
+        }
+      })
+      return this.options
+    },
+    createForm () {
+      return Formio.createForm(this.element, this.project.concat('/', this.formPath), this.options)
     }
-    
-    let config = getFormConfig();
-    
-    let translationsUrl = config.project.concat('/', config.translations.path, '/submission');
-    let filter = '?limit=1000&select=data.label,data.' + config.translations.language;
-    
-    Formio.request(translationsUrl + filter, 'get').then(function(items) {
-        let lang = config.translations.language;
-        let i18n = {};
-        i18n[lang] = {}
-        for(let i = 0; i < items.length; i++) {
-            let item = items[i];
-            if (item.data.hasOwnProperty(lang)) {
-                i18n[lang][item.data.label] = item.data[lang];
-            }
-        }
-        return {
-            language: lang,
-            i18n: i18n
-        }
-    }).then(function(options) {
-        Formio.createForm(document.getElementById('formio'), config.project.concat('/', config.formPath), options).then(function(form) {
-            document.getElementById('spinner').remove();
-            form.language = options.language;
-            form.on('submitDone', function(submission) {
-                let title = options.i18n.resources[options.language]['Sent!'] || 'Sent!';
-                let msg = options.i18n.resources[options.language]['Your submission has been sent!'] || 'Your submission has been sent!';
-                let formio = document.getElementById('formio');
-                formio.innerHTML = `<div class="alert alert-success alert-submitted col-md-6 col-md-offset-3"><strong><span class="glyphicon glyphicon-ok-sign"></span> ${title}</strong><br/>${msg}</div>`;
-            });
-        });
-    }).catch(function(error) {
-        console.log('form rendering error: ', error);
-    });
-}
+  },
+  computed: {
+    element () {
+      return document.getElementById('formio')
+    }
+  },
+  created () {
+    let configString = atob(this.getParameterByName('form'))
+    this.project = this.getParameterByName('project', configString) || 'https://examples.form.io'
+    this.formPath = this.getParameterByName('path', configString) || 'example'
+    this.translationsPath = this.getParameterByName('i18n', configString) || 'translations'
+    this.options.language = this.getParameterByName('lang') || 'en'
+    this.options.i18n[this.options.language] = {}
+    this.showHeader = this.getParameterByName('header') ? parseInt(this.getParameterByName('header')) : true
+    this.createTranslations().then((options) => {
+      this.createForm().then((form) => {
+        this.loading = false
+        form.language = options.language
+      })
+    })
+  }
+})
---
+++ scripts/app.legacy.js
@@ -0,0 +1,64 @@
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    let regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
+    let results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function getFormConfig() {
+    let configString = atob(getParameterByName('form'));
+    return {
+        project: getParameterByName('project', configString) || 'https://examples.form.io',
+        formPath: getParameterByName('path', configString) || 'example',
+        translations: {
+            language: getParameterByName('lang') || 'en',
+            path: getParameterByName('i18n', configString) || 'translations'
+        }
+    }
+}
+
+window.onload = function() {
+    let header = document.getElementById('header');
+    let showHeader = parseInt(getParameterByName('header'));
+    
+    if (!showHeader) {
+        header.remove();
+    }
+    
+    let config = getFormConfig();
+    
+    let translationsUrl = config.project.concat('/', config.translations.path, '/submission');
+    let filter = '?limit=1000&select=data.label,data.' + config.translations.language;
+    
+    Formio.request(translationsUrl + filter, 'get').then(function(items) {
+        let lang = config.translations.language;
+        let i18n = {};
+        i18n[lang] = {}
+        for(let i = 0; i < items.length; i++) {
+            let item = items[i];
+            if (item.data.hasOwnProperty(lang)) {
+                i18n[lang][item.data.label] = item.data[lang];
+            }
+        }
+        return {
+            language: lang,
+            i18n: i18n
+        }
+    }).then(function(options) {
+        Formio.createForm(document.getElementById('formio'), config.project.concat('/', config.formPath), options).then(function(form) {
+            document.getElementById('spinner').remove();
+            form.language = options.language;
+            form.on('submitDone', function(submission) {
+                let title = options.i18n.resources[options.language]['Sent!'] || 'Sent!';
+                let msg = options.i18n.resources[options.language]['Your submission has been sent!'] || 'Your submission has been sent!';
+                let formio = document.getElementById('formio');
+                formio.innerHTML = `<div class="alert alert-success alert-submitted col-md-6 col-md-offset-3"><strong><span class="glyphicon glyphicon-ok-sign"></span> ${title}</strong><br/>${msg}</div>`;
+            });
+        });
+    }).catch(function(error) {
+        console.log('form rendering error: ', error);
+    });
+}
